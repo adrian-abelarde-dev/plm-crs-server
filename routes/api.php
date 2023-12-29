f7acdf00b283e216f0de245c94a8e026294536b3
urquico <kurtjacob.urquico@gmail.com>
2023-12-29 15:59:07 +0800

    [Refactor][Kurt] - Removed Email Blast config

--- routes/api.php
+++ routes/api.php
@@ -38,45 +38,6 @@ Route::post('/users', [UsersController::class, 'insertUser']);
 Route::put('/user/1/{userId}', [UsersController::class, 'updateUser']);
 
 
-// Email blast:
-// Route for /api/email-blast --> Send email to all users
-Route::post('/email-blast', function () {
-    //Create an instance; passing `true` enables exceptions
-    $mail = new PHPMailer(true);
-
-    try {
-        //Server settings
-        $mail->isSMTP(); //Send using SMTP
-        $mail->Host       = 'smtp.example.com'; //Set the SMTP server to send through
-        $mail->SMTPAuth   = true; //Enable SMTP authentication
-        $mail->Username   = ''; //SMTP username
-        $mail->Password   = ''; //SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; //Enable implicit TLS encryption
-        $mail->Port       = 465; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-        //Recipients
-        $mail->setFrom('from@example.com', 'Mailer');
-        $mail->addAddress('', '');     //Add a recipient
-        $mail->addReplyTo('info@example.com', 'Information');
-
-        //Content
-        $mail->isHTML(true); //Set email format to HTML
-        $mail->Subject = ' Email Blast test';
-        $mail->Body    = 'Test email blast';
-        $mail->AltBody = 'Test email blast';
-
-        $mail->send();
-        echo 'Message has been sent';
-        return response()->json(['message' => 'Email sent successfully!']);
-    } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-        return response()->json(['error' => 'Error sending email!', 'errData' => $e]);
-    }
-    
-    
-});
-
-
 
 
 
--- routes/emailBlast.api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\EmailBlastController;
-
-// POST route for sending email credentials
-Route::post('/email-blast', [EmailBlastController::class, 'sendCredentials']);

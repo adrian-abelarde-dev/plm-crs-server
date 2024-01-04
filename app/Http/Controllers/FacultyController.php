8bf7df8cbdc547e95b2dcc35452dac7c63b52db5
WaldorfChristianManalili <wmanalili.simplevia@gmail.com>
2024-01-05 04:00:44 +0800

    [Feature] Faculty added for both undergrad and grad based on ERD

--- app/Http/Controllers/FacultyController.php
+++ app/Http/Controllers/FacultyController.php
@@ -3,9 +3,77 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Faculty;
+use App\Models\Users;
 use Illuminate\Http\Request;
 
 class FacultyController extends Controller
 {
-    //
+    public function getAllFaculty()
+    {
+        // Retrieve all faculty members
+        $faculty = Faculty::get();
+
+        return response()->json(['Faculty' => $faculty]);
+    }
+
+    public function searchFaculty($facultyId)
+    {
+        // Retrieve all faculty members
+        $faculty = Faculty::where('faculties.id', $facultyId)->get();
+
+        return response()->json(['Faculty' => $faculty]);
+    }
+
+    public function insertFaculty(Request $request, $userId, $facultyId)
+    {
+        //check if userId exists
+        if (Users::where('id', $userId)->exists()) {
+            // Validate the request data (customize these rules based on your requirements)
+            $request->validate([
+                'tinNumber' => 'required',
+                'gsisNumber' => 'required',
+                'pedigree' => 'required',
+                'instructorCode' => 'required',
+                'onGraduate' => 'required|boolean',
+            ]);
+
+            // Create a new Faculty member
+            $faculty = new Faculty([
+                'id' => $facultyId,
+                'userId' => $userId,
+                'tinNumber' => $request->input('tinNumber'),
+                'gsisNumber' => $request->input('gsisNumber'),
+                'pedigree' => $request->input('pedigree'),
+                'instructorCode' => $request->input('instructorCode'),
+                'onGraduate' => $request->input('onGraduate')
+            ]);
+
+            $faculty->save();
+
+            return response()->json(['message' => 'Faculty member inserted successfully']);
+        } else {
+            return response()->json(['message' => 'User not found']);
+        }
+    }
+
+    public function updateFaculty(Request $request, $facultyId)
+    {
+        // Validate the request data
+        $request->validate([
+            'tinNumber' => 'required',
+            'gsisNumber' => 'required',
+            'pedigree' => 'required',
+            'instructorCode' => 'required',
+            'onGraduate' => 'required|boolean',
+        ]);
+
+        // Find the faculty by id
+        $find = Faculty::findOrFail($facultyId);
+
+        // Update faculty data
+        $find->update($request->all());
+
+        return response()->json(['message' => 'Faculty member updated successfully']);
+    }
 }
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -42,7 +42,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->middleware('api')
                 ->namespace($this->namespace)
                 // ->group(base_path('routes/api.php'));
-                ->group(function($router){
+                ->group(function ($router) {
                     require base_path('routes/api.php');
                     require base_path('routes/activities.api.php');
                     require base_path('routes/blocks.api.php');
@@ -51,6 +51,7 @@ class RouteServiceProvider extends ServiceProvider
                     require base_path('routes/meetingTypes.api.php');
                     require base_path('routes/programs.api.php');
                     require base_path('routes/userTypes.api.php');
+                    require base_path('routes/faculty.api.php');
                 });
 
             Route::middleware('web')

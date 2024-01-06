d9bab98da8d644f084d88e4c99fea281209675f2
urquico <44643247+urquico@users.noreply.github.com>
2024-01-06 15:11:00 +0800

    [Feature][Kurt] - Added `Admin-Activities` API Routes (#3)
    * [Feature][Kurt] - Added creation of users
    
    * [Feature][Kurt] - Added return message when email already exists
    
    * [Fix][Kurt] - Fixed insertion of plmEmail and userId
    
    * [Feature][Kurt] - Added Edit User
    
    * [Feature][Kurt] - Added insertion of multiple userTypes
    
    * [Feature][Kurt] - Added return roles
    
    * [Refactor][Kurt] -Refactor login() code
    
    * [Feature][Kurt] - Added error message when userId already exists
    
    * [Feature][Kurt] - Added model and controller for activities
    
    * [Feature][Kurt] - Added creation of single activities
    
    * [Feature][Kurt] - Added update activity
    
    * [Feature][Kurt] - Added getting /all activities
    
    * [Feature][Kurt] - Added get 1 activity
    
    * [Feature][Kurt] - Added deleteActivity and model & controller for particioants
    
    * [Feature][Kurt] - Added DELETE and GET for participants

---
+++ app/Http/Controllers/ActivitiesController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Activities;
+use Illuminate\Support\Facades\Validator;
+
+class ActivitiesController extends Controller
+{
+    public function getAllActivities()
+    {
+        $activities = Activities::all();
+
+        return response()->json(['activities' => $activities]);
+    }
+
+    public function getActivityById($id)
+    {
+        $activity = Activities::find($id);
+
+        if (!$activity) {
+            return response()->json(['message' => 'Activity not found'], 404);
+        }
+
+        return response()->json(['activity' => $activity]);
+    }
+
+    public function createActivity(Request $request)
+    {
+        // Validate the request data
+        $validator = Validator::make($request->all(), [
+            'activityName' => 'required',
+            'aysem' => 'required',
+            'dateRange' => 'required|json',
+            'startTime' => 'required',
+            'endTime' => 'required',
+            'status' => 'required',
+        ]);
+
+        // If validation fails, return errors
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
+        // Create a new activity
+        $activity = Activities::create([
+            'activityName' => $request->input('activityName'),
+            'aysem' => $request->input('aysem'),
+            'dateRange' => $request->input('dateRange'), // Decode JSON to an array
+            'startTime' => $request->input('startTime'),
+            'endTime' => $request->input('endTime'),
+            'status' => $request->input('status'),
+        ]);
+
+        return response()->json(['message' => 'Activity created successfully', 'activity' => $activity], 201);
+    }
+
+    public function updateActivity(Request $request, $id)
+    {
+        // Validate the request data
+        $validator = Validator::make($request->all(), [
+            'activityName' => 'required',
+            'aysem' => 'required',
+            'dateRange' => 'required|json',
+            'startTime' => 'required',
+            'endTime' => 'required',
+            'status' => 'required',
+        ]);
+
+        // If validation fails, return errors
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
+        // Find the activity by its ID
+        $activity = Activities::find($id);
+
+        // If the activity doesn't exist, return a not found response
+        if (!$activity) {
+            return response()->json(['message' => 'Activity not found'], 404);
+        }
+
+        // Update the activity with the new data
+        $activity->update([
+            'activityName' => $request->input('activityName'),
+            'aysem' => $request->input('aysem'),
+            'dateRange' => $request->input('dateRange'),
+            'startTime' => $request->input('startTime'),
+            'endTime' => $request->input('endTime'),
+            'status' => $request->input('status'),
+        ]);
+
+        return response()->json(['message' => 'Activity updated successfully', 'activity' => $activity]);
+    }
+
+    public function deleteActivity($id)
+    {
+        $activity = Activities::find($id);
+
+        if (!$activity) {
+            return response()->json(['message' => 'Activity not found'], 404);
+        }
+
+        $activity->delete();
+
+        return response()->json(['message' => 'Activity deleted successfully']);
+    }
+}

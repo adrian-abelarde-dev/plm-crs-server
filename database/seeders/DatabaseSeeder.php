a4c1debd989f7d46c78acd13f4bbe91a8d6e8664
WaldorfChristianManalili <wmanalili.simplevia@gmail.com>
2024-01-12 02:15:32 +0800

    [Feature][Waldorf] College grad teaching assignments

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,6 +9,8 @@ use App\Models\Users;
 use App\Models\College;
 use App\Models\Program;
 use App\Models\StudentTerm;
+use App\Models\Faculty;
+use App\Models\GradClasses;
 
 class DatabaseSeeder extends Seeder
 {
@@ -101,6 +103,104 @@ class DatabaseSeeder extends Seeder
             'isEnrolled' => false,
             'scholasticStatus' => 'Not Paying',
             'isGraduating' => true
+        ]);
+
+        //Faculties
+        Users::insert([
+            'id' => 19901010,
+            //'userType' => ['Student', 'StudentGrad', 'ChairpersonGrad', 'Faculty'],
+            'firstName' => 'Sir',
+            'middleName' => 'Dela',
+            'lastName' => 'Cruz',
+            'plmEmail' => 'sdcruz2020@plm.edu.ph'
+        ]);
+
+        Users::insert([
+            'id' => 19901011,
+            //'userType' => ['Student', 'StudentGrad', 'ChairpersonGrad', 'Faculty'],
+            'firstName' => 'Maam',
+            'middleName' => 'Dela',
+            'lastName' => 'Cruz',
+            'plmEmail' => 'maamdcruz2020@plm.edu.ph'
+        ]);
+
+        Faculty::insert([
+            'id' => 19901010, //facultyId
+            'userid' => 19901010,
+            //'userType' => ['Student', 'StudentGrad', 'ChairpersonGrad', 'Faculty'],
+            'tinNumber' => '000-123-456-001',
+            'gsisNumber' => '123-123-456-001',
+            'pedigree' => 'sample',
+            'instructorCode' => 'ABC123',
+            'onGraduate' => true
+        ]);
+
+        Faculty::insert([
+            'id' => 19901011, //facultyId
+            'userid' => 19901011,
+            //'userType' => ['Student', 'StudentGrad', 'ChairpersonGrad', 'Faculty'],
+            'tinNumber' => '111-222-333-444',
+            'gsisNumber' => '222-111-444-333',
+            'pedigree' => 'sample2',
+            'instructorCode' => 'CDE123',
+            'onGraduate' => true
+        ]);
+
+        //Collegegrad-classes
+        GradClasses::insert([
+            'courseCode' => 'CSC101',
+            'section' => 'A',
+            'courseTitle' => 'Advanced Computer Architecture 1',
+            'units' => '3.0',
+            'classSchedule' => 'MWF 10:00AM - 11:00AM',
+            'studentCount' => '27',
+            'creditedUnits' => '3.0',
+            'college' => 'College of Engineering and Technology',
+            'loadType' => 'RL',
+            'aysem' => '20212',
+            'facultyId' => '19901010'
+        ]);
+
+        GradClasses::insert([
+            'courseCode' => 'CSC101',
+            'section' => 'A',
+            'courseTitle' => 'Advanced Computer Architecture 1',
+            'units' => '3.0',
+            'classSchedule' => 'MWF 10:00AM - 11:00AM',
+            'studentCount' => '30',
+            'creditedUnits' => '3.0',
+            'college' => 'College of Engineering and Technology',
+            'loadType' => 'RL',
+            'aysem' => '20212',
+            'facultyId' => '19901011'
+        ]);
+
+        GradClasses::insert([
+            'courseCode' => 'CMSC 11',
+            'section' => 'S11',
+            'courseTitle' => 'Introduction to Computer Science',
+            'units' => '3.0',
+            'classSchedule' => 'MWF 10:00AM - 11:00AM',
+            'studentCount' => '30',
+            'creditedUnits' => '3.0',
+            'college' => 'College of Engineering and Technology',
+            'loadType' => 'RL',
+            'aysem' => '20212',
+            'facultyId' => '19901010'
+        ]);
+
+        GradClasses::insert([
+            'courseCode' => 'CMSC 22',
+            'section' => 'S22',
+            'courseTitle' => 'Data Structures and Algorithms',
+            'units' => '3.0',
+            'classSchedule' => 'T 9:00AM-12:00PM',
+            'studentCount' => '25',
+            'creditedUnits' => '3.0',
+            'college' => 'College of Engineering and Technology',
+            'loadType' => 'EL',
+            'aysem' => '20212',
+            'facultyId' => '19901010'
         ]);
     }
 
